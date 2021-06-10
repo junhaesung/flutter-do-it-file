ecaafc928ed6fe0f306520ac7110d688b5771d08
Haeseong Jeon <seong0428@gmail.com>
2021-06-11 00:54:44 +0900

    2단계: FileApp 클래스 추가

---
+++ lib/fileApp.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class FileApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _FileApp();
+}
+
+class _FileApp extends State<FileApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('File Example'),
+      ),
+      body: Container(),
+    );
+  }
+}
